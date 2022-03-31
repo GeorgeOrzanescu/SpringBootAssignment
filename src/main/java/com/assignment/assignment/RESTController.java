489d0235f16c1f7b9560f7586dec2f4f61f6166f
GeorgeOrzanescu <george.asro@gmail.com>
2022-03-31 17:51:21 +0300

    test

--- src/main/java/com/assignment/assignment/RESTController.java
+++ src/main/java/com/assignment/assignment/RESTController.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RESTController {
@@ -18,8 +15,8 @@ public class RESTController {
     private LinkRepository linkRepository;
 
     @PostMapping("/url")
-    public String createLink(@PathVariable String url) {
-        List<Link> links = (List<Link>) linkRepository.findAll();
+    public String createLink(@RequestBody Map<String, String> values) {
+        Map<Integer,Link> links =   (Map<Integer,Link>) linkRepository.findAll();
 
         if (url.isEmpty()) { // basic validation
             return "No URL provided";
