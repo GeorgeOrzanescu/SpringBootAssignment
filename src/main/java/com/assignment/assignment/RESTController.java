c71918e63b711a6608b3a49343a19be3087ac7db
GeorgeOrzanescu <george.asro@gmail.com>
2022-03-31 19:05:34 +0300

    Work in progress bug at get
    Signed-off-by: GeorgeOrzanescu <george.asro@gmail.com>

--- src/main/java/com/assignment/assignment/RESTController.java
+++ src/main/java/com/assignment/assignment/RESTController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,14 +16,10 @@ public class RESTController {
     @Autowired
     private LinkRepository linkRepository;
 
-    @PostMapping("/url")
-    public String createLink(@RequestBody Map<String, String> values) {
-        Map<Integer,Link> links =   (Map<Integer,Link>) linkRepository.findAll();
 
-        if (url.isEmpty()) { // basic validation
-            return "No URL provided";
-        }
-
+    private int checkUrl(String url) {
+        ArrayList<Link> links = (ArrayList<Link>)linkRepository.findAll();
+        System.out.println(links.size());
         boolean isPresent = false;
         int indexPresent = 0;
         for(Link link : links) {
@@ -31,27 +29,40 @@ public class RESTController {
             }
         }
         if(isPresent) {
-            return links.get(indexPresent).getShortAddress();
+            return indexPresent;
         }
         else {
-            long lastId = links.get(links.size() - 1).getId(); // get last id
-            String shortAddress = "http://localhost:9003/" + (lastId + 1);
-            Link link = new Link("https://" + url, shortAddress);
-            linkRepository.save(link);
-
-            return shortAddress;
+            return -1;
         }
+    }
+
+    @PostMapping("/url")
+    public List<String> createLink(@RequestBody Map<String, String> values) {
+        List<String> generatedLinks = new ArrayList<>();
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+
+                int index = checkUrl(entry.getValue());
+                 if(index == -1) {
+                     String shortAddress = "http://localhost:9003/" + Long.toString(linkRepository.count()+1);
+                     Link link = new Link(entry.getValue(),shortAddress);
+                     linkRepository.save(link);
+                     generatedLinks.add(shortAddress);
+                 }
+                 else {
+                     generatedLinks.add("http://localhost:9003/" + Long.toString(index));
+                 }
+        }
+        return generatedLinks;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getLink(@PathVariable Integer id) {
-        List<Link> links = (List<Link>)linkRepository.findAll();
-        if(id > links.size() || id <= 0) {
+        if(id > linkRepository.count() || id <= 0) {
             return new ResponseEntity("No such link", HttpStatus.NOT_FOUND);
         }
         else{
             HttpHeaders headers = new HttpHeaders();
-            headers.set("Location", links.get(id-1).getLongAddress());
+            headers.set("Location", linkRepository.findById(Long.valueOf(id)).get().getLongAddress());
             ResponseEntity<?> responseEntity = new ResponseEntity<>(headers, HttpStatus.PERMANENT_REDIRECT);
             return responseEntity;
         }
